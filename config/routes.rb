42050bf4feb02d270f3a18d0d844ffa147a78157
Quill Cronwall <quilljazz@gmail.com>
2021-08-23 18:56:48 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/index'
-  get 'messages/create'
-  get 'messages/index'
-  get 'participants/create'
-  get 'participants/index'
-  get 'events/create'
-  get 'events/index'
-  get 'kurasus/create'
-  get 'kurasus/edit'
-  get 'kurasus/index'
+  resources :kurasus, only: %i[index, edit, create, new]
+  resources :events, only: %i[index, create, new] do
+    resources :participants, only: %i[index, create, new]
+  end
+
+  resources :students, only: [] do
+    resources :messages, only: %i[index, create, new]
+  end
+  resources :messages, only: %i[show] do
+    resources :comments, only: %i[create, new]
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
