991ab2e1090c5ea6e1dcbaaf642a73a09a21f275
Quill Cronwall <quilljazz@gmail.com>
2021-08-24 14:44:41 +0900

    add pundit to gemfile and policy kurasu, comment

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,10 +1,13 @@
 class ParticipantsController < ApplicationController
   def create
+    authorize @participant
   end
 
   def new
+    authorize @participant
   end
 
   def index
+    authorize @participant
   end
 end
