e37b55018312cf688af1b53a0535ddb0faf42183
Quill Cronwall <quilljazz@gmail.com>
2021-08-24 15:27:16 +0900

    create dashboard controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashboard/show'
   resources :kurasus, only: %i[index, edit, create, new]
   resources :events, only: %i[index, create, new] do
     resources :participants, only: %i[index, create, new]
@@ -10,7 +11,9 @@ Rails.application.routes.draw do
   resources :messages, only: %i[show] do
     resources :comments, only: %i[create, new]
   end
+  resources :dashboard, only: %i[index]
+  # /dashboard
   devise_for :users
-  root to: 'pages#home'
+  root to: 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
