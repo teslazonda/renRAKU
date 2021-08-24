1d8b82e71e0f178433134fff3b0d9343b28cda5c
Sae Shikiji <saeshikiji@gmail.com>
2021-08-24 17:26:02 +0900

    Added action text and styled the form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,7 +24,7 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :content, :date, participants_attributes: [:kurasu_id])
+    params.require(:event).permit(:title, :rich_body, :content, :date, participants_attributes: [:kurasu_id])
   end
 
 end
