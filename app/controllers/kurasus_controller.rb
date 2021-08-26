42808748d737e2d76ce959ff0f905eb6c3ef6199
Quill Cronwall <quilljazz@gmail.com>
2021-08-26 15:12:09 +0900

    authorize kurasu

--- app/controllers/kurasus_controller.rb
+++ app/controllers/kurasus_controller.rb
@@ -1,16 +1,21 @@
 class KurasusController < ApplicationController
   def create
+    authorize @kurasu
   end
 
   def new
+    authorize @kurasu
   end
 
   def edit
+    authorize @kurasu
   end
 
   def update
+    authorize @kurasu
   end
 
   def index
+    @user = current_user
   end
 end
