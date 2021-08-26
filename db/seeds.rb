24aff3380271109876dbf43bd428bb0f086a59a3
David Gregorato <HydroSnake@gmail.com>
2021-08-26 16:51:35 +0900

    Reduced the number of parents, commented out the special parents.

--- db/seeds.rb
+++ db/seeds.rb
@@ -88,19 +88,19 @@ viddy = User.create!(
 )
 
 # Creating two extra parents in case we need specific parents for demonstration purposes.
-quill = User.create!(
-  email: "quill@me.com",
-  password: 123456,
-  name: "Quill",
-  teacher: false
-)
+# quill = User.create!(
+#   email: "quill@me.com",
+#   password: 123456,
+#   name: "Quill",
+#   teacher: false
+# )
 
-lena = User.create!(
-  email: "lena@me.com",
-  password: 123456,
-  name: "Lena",
-  teacher: false
-)
+# lena = User.create!(
+#   email: "lena@me.com",
+#   password: 123456,
+#   name: "Lena",
+#   teacher: false
+# )
 
 parents = []
 239.times do
