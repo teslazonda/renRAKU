cc6fc373fcfa30f391ccfaeccd75d0c6a92342c6
David Gregorato <HydroSnake@gmail.com>
2021-08-26 14:48:21 +0900

    cleaned the model dependencies.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :message
-  # belongs_to :user, through: :messages
   validates :content, presence: true
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,6 @@
 class Message < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :student
-  # belongs_to :user, through: :students
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
