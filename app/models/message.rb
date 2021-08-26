82dd1222bd73cf4338889ea38603d0d75fd1d6cc
David Gregorato <HydroSnake@gmail.com>
2021-08-26 14:13:22 +0900

    Changed seeds and worked on linking the different tables.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   has_many :comments
   belongs_to :student
+  belongs_to :user, through: :students
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
