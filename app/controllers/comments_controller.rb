09be1f174b46c7a7ecc469f5284b625c1dc937c5
David Gregorato <HydroSnake@gmail.com>
2021-08-28 01:05:10 +0900

    Fixed the new comment redirect.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :find_message, only: %i[create new]
   def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.message = @message
     authorize @comment
+    if @comment.save
+      redirect_to student_messages_path(@message.student_id)
+    else
+      raise
+      render 'comments/new'
+    end
   end
 
   def new
-    @message = Message.find(params[:message_id])
     @comment = Comment.new
     authorize @comment
+  end
+
+  private
+
+  def find_message
+    @message = Message.find(params[:message_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id)
   end
 end
