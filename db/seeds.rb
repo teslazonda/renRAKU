23d968a5b2c570d7f5e40a1df53232cb79f913f8
Sae Shikiji <saeshikiji@gmail.com>
2021-08-30 11:56:02 +0900

    added one meeting to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -498,3 +498,11 @@ Comment.create(
   message_id: lunch.id,
   content: "Thank you!"
 )
+
+Meeting.create(
+  title: "Needs Extra Study",
+  context: "Ren-kun seems to be struggling with the prepositions topic in English. I would like to meet you sometime next wednesday for a short meeting.",
+  date: 2021-9-22,
+  parent_id: viddy.id,
+  teacher_id: sae.id
+)
