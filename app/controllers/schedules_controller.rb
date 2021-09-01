b3d698a6a4c8ef3f8b5f9a8240b2a448f6c5ea00
Sae Shikiji <saeshikiji@gmail.com>
2021-09-02 08:46:54 +0900

    Added notification for uploading a class schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -17,6 +17,7 @@ class SchedulesController < ApplicationController
     @schedule.kurasu = @kurasu
     if @schedule.save
       redirect_to kurasu_schedules_path(@kurasu)
+      LineCreator.new("New class schedule was uploaded").send_line
     else
       render '/schedules/new'
     end
