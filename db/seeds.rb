7bdf23d357c05c84b8dba1dc414896fe7fcb1582
Sae Shikiji <saeshikiji@gmail.com>
2021-09-01 14:24:32 +0900

    Added Japanese students to 1-A seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -305,11 +305,7 @@ Grade.create!(
   student: ren
 )
 
-
 puts "Creating 9 students for class 1-A"
-
-# counter = 1
-parent_counter = 0
 
 Student.create!(
   first_name: 'Ayaka',
@@ -317,7 +313,7 @@ Student.create!(
   birthday: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'),
   student_number: 2,
   kurasu_id: class_one_a.id,
-  user_id: parents[parent_counter].id
+  user_id: parents[0].id
 )
 
 Student.create!(
@@ -326,7 +322,7 @@ Student.create!(
   birthday: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'),
   student_number: 3,
   kurasu_id: class_one_a.id,
-  user_id: parents[parent_counter].id
+  user_id: parents[1].id
 )
 
 Student.create!(
@@ -335,7 +331,7 @@ Student.create!(
   birthday: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'),
   student_number: 4,
   kurasu_id: class_one_a.id,
-  user_id: parents[parent_counter].id
+  user_id: parents[2].id
 )
 
 Student.create!(
@@ -344,7 +340,7 @@ Student.create!(
   birthday: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'),
   student_number: 5,
   kurasu_id: class_one_a.id,
-  user_id: parents[parent_counter].id
+  user_id: parents[3].id
 )
 
 Student.create!(
@@ -353,7 +349,7 @@ Student.create!(
   birthday: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'),
   student_number: 6,
   kurasu_id: class_one_a.id,
-  user_id: parents[parent_counter].id
+  user_id: parents[4].id
 )
 
 Student.create!(
@@ -362,7 +358,7 @@ Student.create!(
   birthday: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'),
   student_number: 7,
   kurasu_id: class_one_a.id,
-  user_id: parents[parent_counter].id
+  user_id: parents[5].id
 )
 
 Student.create!(
@@ -371,7 +367,7 @@ Student.create!(
   birthday: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'),
   student_number: 8,
   kurasu_id: class_one_a.id,
-  user_id: parents[parent_counter].id
+  user_id: parents[6].id
 )
 
 Student.create!(
@@ -380,39 +376,18 @@ Student.create!(
   birthday: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'),
   student_number: 9,
   kurasu_id: class_one_a.id,
-  user_id: parents[parent_counter].id
+  user_id: parents[7].id
 )
 
+puts "Created Students in 1-A"
 
-9.times do
-  # counter += 1
-  # student = Student.create!(
-  #   first_name: Faker::Name.first_name,
-  #   last_name: Faker::Name.last_name,
-  #   birthday: Faker::Date.between(from: '2015-01-01', to: '2015-12-31'),
-  #   student_number: counter,
-  #   kurasu_id: class_one_a.id,
-  #   user_id: parents[parent_counter].id
-  # )
-
-  5.times do
-  Grade.create!(
-    date: grade_dates.sample,
-    subject: grade_subject.sample,
-    name: grade_name.sample,
-    value: grade_value.sample,
-    student: student
-  )
-  end
-  parent_counter += 1
-  puts "Created #{student.id}"
-end
 
 puts "students created"
 
 puts "Creating 10 students for class 1-B"
 
-# counter = 10
+counter = 10
+parent_counter = 8
 10.times do
   counter += 1
   student = Student.create!(
